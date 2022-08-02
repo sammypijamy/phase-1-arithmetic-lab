5fbc6d54175c5e66571c24448839261ac9008f88
sammypijamy <samaragransky@outlook.com>
2022-08-01 22:59:33 -0700

    commit tree 25d79b3cfc713108b6b940a8b9316df32c33181d

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const num1 = 31
+const num2 = 2
+const multiply = (num1 * num2);
 
-// Write your code here
+
+const random = Math.floor(Math.random() * 10) +5;
+
+const num3 = 8 ;
+const num4 = 2;
+const mod = (num3 / num4);
+
+
+const max = Math.max( 2, 5, 8, 20) 
+
